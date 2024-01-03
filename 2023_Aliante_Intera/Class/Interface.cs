75f6d33882d613e8a79fba5ad9fe9484b89ce5a8
azzol <azzol@PC001>
2024-01-03 16:18:26 +0100

    Interfece

---
+++ 2023_Aliante_Intera/Class/Interface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023_Aliante_Intera
+{
+    public interface IComposite
+    {
+        void Add(IComposite componente);
+        void Remove(int index);
+        IComposite Get(int index);
+        bool Equals(object obj);
+        string ToString();
+        double Price();
+    }
+}
