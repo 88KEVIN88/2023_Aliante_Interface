b67dbccdc62103739aa021f2c8e95e3eb17b8d01
azzol <azzol@PC001>
2024-01-03 17:23:32 +0100

    Classe Cerchione e relativi metodi

--- 2023_Aliante_Intera/Class/Cerchione.cs
+++ 2023_Aliante_Intera/Class/Cerchione.cs
@@ -6,7 +6,76 @@ using System.Threading.Tasks;
 
 namespace _2023_Aliante_Intera
 {
-    internal class Cerchione
+    internal class Cerchione : IComposite
     {
+        private double _pollici;
+        private string _materiale;
+
+        public double Pollici
+        {
+            get { return _pollici; }
+            set
+            {
+                if(value > 0 && value < 10)
+                {
+                    _pollici = value;
+                }
+                else
+                {
+                    throw new ArgumentException("La dimensione deve esseere > 0");
+                }
+            }
+        }
+        public string Materiale
+        {
+            get { return _materiale; }
+            set { _materiale = value; }
+        }
+        public Cerchione()
+        {
+            Pollici = 0;
+            Materiale = string.Empty;
+        }
+        public Cerchione(double pollici, string materiale)
+        {
+            Pollici = pollici;
+            Materiale = materiale;
+        }
+        public Cerchione(Cerchione vecchio)
+        {
+            Pollici = vecchio.Pollici;
+            Materiale = vecchio.Materiale;
+        }
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Cerchione) || obj == null)
+            {
+                return false;
+            }
+
+            Cerchione other = (Cerchione)obj;
+            return Pollici == other.Pollici && Materiale == other.Materiale;
+        }
+        public void Add(IComposite componente)
+        {
+
+        }
+        public void Remove(int index)
+        {
+
+        }
+        public IComposite Get(int index)
+        {
+            return null;
+        }
+        public override string ToString()
+        {
+            return $"Pollici: {Pollici}; Materiale:{Materiale}";
+        }
+        public double Price()
+        {
+            return Pollici;
+        }
+            
     }
 }
