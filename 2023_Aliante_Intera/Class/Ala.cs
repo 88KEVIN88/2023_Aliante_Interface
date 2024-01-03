46fa9563b9eb9ce672b609dafd43e98e6ab177b6
azzol <azzol@PC001>
2024-01-03 16:35:08 +0100

    Controlli

--- 2023_Aliante_Intera/Class/Ala.cs
+++ 2023_Aliante_Intera/Class/Ala.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace _2023_Aliante_Intera
 {
-    internal class Ala 
+    internal class Ala : IComposite
     {
+        private double _lenght;
+        private double _width;
+
+        public double Lenght
+        {
+            get { return _lenght; }
+            set
+            {
+                if(value >= 0)
+                {
+                    _lenght = value;
+                }
+                else
+                {
+                    throw new ArgumentException("La lunghezza deve esseere > 0");
+                }
+            }
+        }
+        public double Width
+        {
+            get { return _width; }
+            set
+            {
+                if(value * 2F >= Lenght)
+                {
+                    _width = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Il valore della larghezza delle ali deve essere > 0");
+                }
+            }
+        }
+        
     }
 }
