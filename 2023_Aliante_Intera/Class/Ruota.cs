1211a94fcd76665fe9f052d67f0b6b07fbb43ecc
azzol <azzol@PC001>
2024-01-03 17:40:32 +0100

    Classe Ruota

--- 2023_Aliante_Intera/Class/Ruota.cs
+++ 2023_Aliante_Intera/Class/Ruota.cs
@@ -6,8 +6,64 @@ using System.Threading.Tasks;
 
 namespace _2023_Aliante_Intera
 {
-    internal class Ruota
+    internal class Ruota : IComposite
     {
+        private Gomma _gomma;
+        private Cerchione _cerchione;
 
+        public Gomma Gomma
+        {
+            get { return _gomma; }
+            set { _gomma = value; }
+        }
+        public Cerchione Cerchione
+        {
+            get { return _cerchione; }
+            set { _cerchione = value; }
+        }
+        public Ruota()
+        {
+            Gomma = new Gomma();   
+            Cerchione = new Cerchione();
+        }
+        public Ruota(Gomma gomma, Cerchione cerchione)
+        {
+            Gomma = gomma;
+            Cerchione = cerchione;
+        }
+        public Ruota(Ruota vecchia)
+        {
+            Gomma = vecchia.Gomma;
+            Cerchione = vecchia.Cerchione;
+        }
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Ruota otherRuota) || otherRuota is null)
+            {
+                return false;
+            }
+
+            return Gomma.Equals(otherRuota.Gomma) && Cerchione.Equals(otherRuota.Cerchione);
+        }
+        public void Add(IComposite componente)
+        {
+
+        }
+        public void Remove(int index)
+        {
+
+        }
+        public IComposite Get(int index)
+        {
+            return null;
+        }
+        public override string ToString()
+        {
+            return $"Descrizione:{Gomma};Cerchioni:{Cerchione};";
+        }
+        public double Price()
+        {
+            return 0;
+        }
     }
 }
