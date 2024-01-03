57a9a0df96620dde098903cfb8760fe76beb5540
azzol <azzol@PC001>
2024-01-03 17:16:16 +0100

    Classe Coda

--- 2023_Aliante_Intera/Class/Coda.cs
+++ 2023_Aliante_Intera/Class/Coda.cs
@@ -8,6 +8,63 @@ namespace _2023_Aliante_Intera
 {
     internal class Coda : IComposite
     {
-       
+        private double _lenght;
+        public double Lenght
+        {
+            get { return _lenght; }
+            set
+            {
+                if (value > 0)
+                {
+                    _lenght = value;
+                }
+                else
+                {
+                    throw new ArgumentException("La lunghezza deve esseere > 0");
+                }
+            }
+        }
+        public Coda()
+        {
+            _lenght = 0;
+        }
+        public Coda(double lenght)
+        {
+            Lenght = lenght;
+        }
+        public Coda(Coda vecchia)
+        {
+            Lenght = vecchia.Lenght;
+        }
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Coda) || obj == null)
+            {
+                return false;
+            }
+
+            Coda other = (Coda)obj;
+            return Lenght == other.Lenght;
+        }
+        public void Add(IComposite componente)
+        {
+
+        }
+        public void Remove(int index)
+        {
+
+        }
+        public IComposite Get(int index)
+        {
+            return null;
+        }
+        public override string ToString()
+        {
+            return  $"Lunghezza coda:{Lenght}";
+        }
+        public double Price()
+        {
+            return _lenght;
+        }
     }
 }
